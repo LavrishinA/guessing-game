027c6d806b7c0b41b04a8417f7918d03cd8d3c16
LavrishinA <iHasher007@gmail.com>
2022-08-15 23:51:50 +0300

    feat: the solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return (this.mid = Math.round((this.right - this.left) / 2 + this.left));
     }
 
     lower() {
-
+        this.right = this.mid;
+        this.guess();
     }
 
     greater() {
-
+        this.left = this.mid;
+        this.guess();
     }
 }
 
